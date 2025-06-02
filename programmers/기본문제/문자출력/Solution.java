5f254ddccac453ab3f4d0d3fff058fbb9840b85e
Lee Jong Won <86555040+bermute@users.noreply.github.com>
2025-06-02 12:09:30 +0900

    Create Solution.java

---
+++ programmers/기본문제/문자출력/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String message = "
+Let's go!
+";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
+
+채점 결과
+정확성: 100.0
+합계: 100.0 / 100.0

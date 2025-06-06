2514489462d92665f8a92e02d5e65f9ca453d509
Lee Jong Won <86555040+bermute@users.noreply.github.com>
2025-06-06 21:09:21 +0900

    Create Solution.java

---
+++ programmers/기본문제/병과분류/Solution.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String code = sc.next();
+        String lastFourWords = code.substring(code.length()-4, code.length());
+
+        if(lastFourWords.equals(
+"_eye"
+)){
+            System.out.println("Ophthalmologyc");
+        }
+        else if(
+lastFourWords.equals("head")
+){
+            System.out.println("Neurosurgery");
+        }
+        else if(
+lastFourWords.equals("infl")
+){
+            System.out.println("Orthopedics");
+        }
+        
+else if (lastFourWords.equals("skin"))
+{
+            System.out.println("Dermatology");
+        }
+        
+else
+{
+            System.out.println("direct recommendation");
+        }
+    }
+}
+
+채점 결과
+정확성: 100.0
+합계: 100.0 / 100.0
